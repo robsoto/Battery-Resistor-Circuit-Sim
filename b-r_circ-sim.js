d32601d135ae72de2abbf96bfb18f078c522374b
Robert Soto <robsoto@mit.edu>
2014-07-02 10:22:00 -0400

    new charge animation

--- b-r_circ-sim.js
+++ b-r_circ-sim.js
@@ -3,7 +3,9 @@
 var R = Raphael(0,0,1500,1500); //Raphael canvas
 
 //circuit skeleton
-var batteryWidth = 300
+var battWidth = 300;
+var battLeft = 290;
+var battRight = 600;
 var battery = R.rect(300,350,300,125);
 var resistor = R.rect(275,135,350,90); 
 var chargeRadius = 10;
@@ -20,6 +22,7 @@ var bottom = 405;
 var pathLen = chargePath.getTotalLength();
 var numCharges = pathLen / ((chargeRadius*2) + 20);
 var subpath = R.path(chargePath.getSubpath(650, 1000)); //subpath within resistor
+var moveFactor = 2;
 subpath.attr({opacity:0});
 chargePath.attr({opacity:0}); 
 resistor.attr({fill:resColor})
@@ -27,10 +30,11 @@ resistor.attr({fill:resColor})
 //creating "charges"
 //var chargeDist = 10;
 //var chargePos = chargePath.getPointAtLength(chargeDist);
-charges = R.set();
-
+//circles = R.set();
+charges = R.set(); 
 function charge() {
 	//chargeDist += chargeRadius + 30;
+	//var counter = 0;
 	this.dist = charges.length * (chargeRadius * 2 + 20);
 	if (this.dist >= pathLen) {
 		this.dist = this.dist % pathLen;	
@@ -38,14 +42,24 @@ function charge() {
 	var chargePos = chargePath.getPointAtLength(this.dist);
 	var c = R.circle(chargePos.x, chargePos.y, chargeRadius);
 	charges.push(c);
+	//counter++;
 }
 
+dist = [];
 for (i = 0; i < numCharges; i++) {
 	//charges.push(R.circle(chargePos.x, chargePos.y, 10));
 	//chargeDist += chargeRadius + 30;
 	var c = new charge();
+	dist[dist.length] = c.dist;
+	//circles.push(c);
+	console.log(c.dist);	
 	//chargePos = chargePath.getPointAtLength(c.dist);
 }
+console.log(dist)
+//console.log(circles[0])
+//console.log(charges[0].dist);
+//charges[0].dist++;
+//console.log(charges[0].dist)
 
 charges.attr({fill:'yellow'});
 //console.log(parseInt(charges[2].attr('cx')));
@@ -78,7 +92,7 @@ function heat(elem) {
 }
 
 //Creating voltage and resistance sliders
-var resistance = new slider(R, 900, 100, 400, 50, 1, 10, 'Resistance');
+var resistance = new slider(R, 900, 100, 400, 50, 1, 100, 'Resistance');
 var voltage = new slider(R, 900, 200, 400, 50, 0, 12, 'Voltage');
 
 var current = voltage.val / resistance.val;
@@ -93,25 +107,59 @@ setInterval(function() {
 //CHANGE TO TAKE IN SET OF CHARGES AND MOVE THEM SIMULTANEOUSLY
 //animation of moving charges
 function animateCharges() {
-	var curX = parseInt(charges[0].attr('cx'));
-	var nextPos = chargePath.getPointAtLength(charges[0].dist + 5);
-	var curY = parseInt(charges[0].attr('cy'));
-	console.log(curX + ', ' + curY);
+	var nullAnim = Raphael.animation({});
+	//var curX = parseInt(charges[0].attr('cx'));
+	var nextPos = chargePath.getPointAtLength(dist[0]+moveFactor);
+	//var curY = parseInt(charges[0].attr('cy'));
+	//console.log(curX + ', ' + curY);
+	if ((dist[0]+moveFactor) > pathLen) {
+		var d = (dist[0]+moveFactor) % pathLen;
+		nextPos = chargePath.getPointAtLength(d);	
+	}
 	
-	if (curX < rightSide && curY == bottom) {
-		charges[0].animate({cx:nextPos.x}, 1);
+	if (nextPos.x > battLeft && nextPos.x < battRight && nextPos.y == bottom) {
+		charges[0].attr({opacity:0});	
+	}
+	else { 
+		charges[0].attr({opacity:1});	
+	}
+	
+	dist[0] += moveFactor;
+	charges[0].animate({cx:nextPos.x, cy:nextPos.y}, moveFactor);
+	
+	for (i = 1; i < numCharges; i++) {
+		var nextPos = chargePath.getPointAtLength(dist[i]+moveFactor);
+		if ((dist[i]+moveFactor) > pathLen) {
+			var d = (dist[i]+moveFactor) % pathLen;
+			nextPos = chargePath.getPointAtLength(d);	
+		}
+		var charge = charges[i];
+		dist[i] += moveFactor;
+		charge.animateWith(charges[i-1], nullAnim, {cx:nextPos.x, cy:nextPos.y}, moveFactor);
+		
+		if (nextPos.x > battLeft && nextPos.x < battRight && nextPos.y == bottom) {
+			charges[i].attr({opacity:0});	
+		}
+		else { 
+			charges[i].attr({opacity:1});	
+		}
+	}
+	/*if (curX < rightSide && curY == bottom) {
+		//charges[0].dist++;
+		dist[0] += moveFactor;
+		charges[0].animate({cx:nextPos.x}, moveFactor);
 	}
 	else if (curX >= rightSide && curY > top) {
-		charges[0].animate({cy:curY-1}, 1);
+		charges[0].animate({cy:nextPos.y}, moveFactor);
 	}
 	else if (curX > leftSide && curY == top) {
-		charges[0].animate({cx:curX-1}, 1);	
+		charges[0].animate({cx:curX-1}, moveFactor);	
 	}
 	else if (curX == leftSide && curY > bottom) {
-		charges[0].animate({cy:curY+1}, 1);	
-	}
+		charges[0].animate({cy:curY+1}, moveFactor);	
+	}*/
 	
-	for (i = 1; i < charges.length; i++) {
+	/*for (i = 1; i < charges.length; i++) {
 		var charge = charges[i];
 		var curX = parseInt(charge.attr('cx'));
 		var curY = parseInt(charge.attr('cy'));
@@ -129,7 +177,7 @@ function animateCharges() {
 		else if (curX == leftSide && curY > bottom) {
 			charge.animateWith(charges[i-1], nullAnim, {cy:curY+1}, 1);	
 		}
-	} 
+	} */
 
 		/*for (i in charges) {
 			var charge = charges[i];
@@ -179,5 +227,5 @@ function runBattery() {
 }
 
 //test run battery
-//runBattery();
+runBattery();
 
